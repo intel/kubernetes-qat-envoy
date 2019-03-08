1820baa52154932ef6cb009f270fa55f8f7d183b
Nakul Dahiwade <nakul.dahiwade@intel.com>
2019-03-08 11:15:43 -0800

    Post Checks for QAT Plugin

--- vagrant/postchecks_qat_plugin.sh
+++ vagrant/postchecks_qat_plugin.sh
@@ -10,10 +10,38 @@
 
 set -o pipefail
 set -o xtrace
+set -o errexit
 
 echo "QAT plugin validation"
-# TODO:
-# - Verify that qat devices are configured properly /etc/c6xx_dev*.conf (NumProcesses, NumberCyInstances and NumberDcInstances in SSH section)
-# - Verify that intel-qat2-plugin daemonset is available
-# - Ensure that intel-qat2-plugin pod has registered the devices with this log entry (Device plugin for cy*_dc* registered)
-# - Ensure that envoy-qat:devel docker image exists.
+
+echo "Ensuring that envoy-qat:devel docker image exists..."
+if [[ -z $(sudo docker images | grep envoy-qat | grep devel) ]]; then
+    echo "envoy-qat:devel docker image doesn't exists"
+    exit 1
+fi
+
+echo "Verifying intel-qat2-plugin daemonset is available..."
+plugin_daemonset=$( kubectl get daemonset | grep intel-qat2-plugin)
+if [[ $plugin_daemonset ]]; then
+   if [[ $(echo $plugin_daemonset | awk '{print $2}') != $(echo $plugin_daemonset | awk '{print $6}') ]]; then
+      echo "Ther Intel QAT daemonset plugin is not available yet"
+      exit 1
+   fi
+else
+    echo "The Intel QAT daemonset plugin is not created"
+    exit 1
+fi
+
+echo "Ensuring that the intel-qat2-plugin pod has registered the devices..."
+for plugin_pod in $(kubectl get pods | grep intel-qat2 | awk '{print $1}'); do
+     if [[ -z $(kubectl logs $plugin_pod | grep "Start server for") ]]; then
+        echo "The Intel QAT daemonset has not started properly"
+        exit 1
+     fi
+     if [[ -z $(kubectl logs $plugin_pod | grep "Device plugin for") ]]; then
+        echo "The QAT devices weren't registered properly"
+        exit 1
+     fi
+done
+
+echo -e " \nPost-checks for qat plugin complete! "
